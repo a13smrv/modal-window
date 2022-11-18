4b38cd4f3c265b5d97cfff75525505ca57b38e33
Alexander Smurov <a13smrv@gmail.com>
2022-11-18 22:53:25 +0100

    feat: make a design for the ModalWindow component
    And also:
    - add an SVG image as an icon for closing the ModalWindow component
    - update the background of the Button component

---
+++ src/components/ModalWindow/ModalWindow.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./ModalWindow.scss";
+import crossSVG from "./../../media/cross.svg";
+
+export function ModalWindow() {
+  return (
+    <div className="modalWindow">
+      <div className="titleBar">
+
+      </div>
+      <div className="body">
+
+      </div>
+      {/* <img src={crossSVG} alt="cross" /> */}
+    </div>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Button } from "./components/Button/Button";
+import { ModalWindow } from "./components/ModalWindow/ModalWindow";
 
 import "./index.scss";
 
 function App() {
   return (
     <div className="app">
-      <Button />
+      {/* <Button /> */}
+      <ModalWindow />
     </div>
   )
 }
